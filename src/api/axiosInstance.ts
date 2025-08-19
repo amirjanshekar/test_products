b7d5ce9b9635ecb756e3b50792b83ef96c4a6320
amirjanshekar <a.janshekar@yahoo.com>
2025-08-19 14:08:06 +0330

    fix: adding environment variable, title and description of app

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://amirjanshekar.pythonanywhere.com/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
 export default axiosInstance;
